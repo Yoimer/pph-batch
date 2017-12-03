d59ed15f18aa349b8ed5a266029f8510ed18f138
Yoimer <ydrf19@gmail.com>
2017-12-03 12:54:58 -0400

    added for example

--- project2-base-code.bat
+++ project2-base-code.bat
@@ -4,7 +4,9 @@ echo Hello %username% ,you are currently logged in at %computername%
 echo It is %time% on %date%
 ver
 echo The IP Address on your system is:
-ipconfig/all|find /I "DHCP"
+ipconfig/all|find /I /C "DHCP habilitado . . . . . . . . . . . . . : s"
+set "var=11111"
+echo %var%
 pause
 
 :: another example
@@ -16,3 +18,7 @@ REM echo Network Connection Test
 REM echo The IP Address on this computer is:
 REM for /f "usebackq tokens=2 delims=:" %%f in (`ipconfig ^| findstr /c:%ip_address_string%`) do echo Your IP Address is: %%f
 REM pause
+
+:: for example
+REM For /F %%A In ('Find /C "abc"^<"C:\Users\abc\Desktop\project\string.txt"') Do (
+    REM Set "mlc=%%A")
