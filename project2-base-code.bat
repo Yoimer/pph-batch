1fe2df470911aece59ab7a8f41a6abf36230cdb2
Yoimer <ydrf19@gmail.com>
2017-12-03 12:23:33 -0400

    added ipconfig/all

--- project2-base-code.bat
+++ project2-base-code.bat
@@ -4,7 +4,7 @@ echo Hello %username% ,you are currently logged in at %computername%
 echo It is %time% on %date%
 ver
 echo The IP Address on your system is:
-ipconfig|find /I "IPV4"
+ipconfig/all|find /I "DHCP"
 pause
 
 :: another example
