04b81e4d86c16b67498bda089cbf3cdc275468be
Yoimer <ydrf19@gmail.com>
2017-12-03 15:12:11 -0400

    detects when DHCP is enabled by showing 1 on screen

--- project2-base-code.bat
+++ project2-base-code.bat
@@ -4,21 +4,11 @@ echo Hello %username% ,you are currently logged in at %computername%
 echo It is %time% on %date%
 ver
 echo The IP Address on your system is:
-ipconfig/all|find /I /C "DHCP habilitado . . . . . . . . . . . . . : s"
-set "var=11111"
-echo %var%
+:: spanish windows version
+::for /f %%a in ('ipconfig/all^|find /I /C "DHCP habilitado . . . . . . . . . . . . . : s"') do set count=%%a
+::echo %count%
+
+:: english windows version
+::for /f %%a in ('ipconfig/all^|find /I /C "DHCP Enabled . . . . . . . . . . . . . : YES"') do set count=%%a
+::echo %count%
 pause
-
-:: another example
-REM @echo off
-REM set ip_address_string="IP Address"
-REM Uncomment the following line when using Windows 7 (with removing "rem")!
-REM set ip_address_string="IPv4"
-REM echo Network Connection Test
-REM echo The IP Address on this computer is:
-REM for /f "usebackq tokens=2 delims=:" %%f in (`ipconfig ^| findstr /c:%ip_address_string%`) do echo Your IP Address is: %%f
-REM pause
-
-:: for example
-REM For /F %%A In ('Find /C "abc"^<"C:\Users\abc\Desktop\project\string.txt"') Do (
-    REM Set "mlc=%%A")
