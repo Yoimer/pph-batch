f7ee89dc8144a6d8ab9d2be4453d7c086e565976
Yoimer <ydrf19@gmail.com>
2017-12-03 20:23:36 -0400

    shows ip with garbage when DHCP is enabled

--- project2-base-code.bat
+++ project2-base-code.bat
@@ -5,10 +5,19 @@ echo It is %time% on %date%
 ver
 echo The IP Address on your system is:
 :: spanish windows version
-::for /f %%a in ('ipconfig/all^|find /I /C "DHCP habilitado . . . . . . . . . . . . . : s"') do set count=%%a
-::echo %count%
+for /f %%a in ('ipconfig/all^|find /I /C "DHCP habilitado . . . . . . . . . . . . . : s"') do set count=%%a
+echo %count%
+if %count% GEQ 1 (
+for /f "tokens=17 delims= " %%a in ('ipconfig/all^|find /I "IPv4"') do echo %%a
+)
+::for /f "tokens=14 delims= " %a in ('ipconfig/all^|find /I "IPv4"') do echo %)
+pause
+
+
 
 :: english windows version
 ::for /f %%a in ('ipconfig/all^|find /I /C "DHCP Enabled. . . . . . . . . . . : Yes"') do set count=%%a
 ::echo %count%
-pause
+::pause
+
+::if %count% GEQ 1 (echo "DHCP is enabled") else (echo "DHCP is disabled")
